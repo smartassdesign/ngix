ebbf93af4c49768fcaefeb6ff09c125d71551f3b
hechtron <jon@smartassdesign.com>
2016-01-12 18:51:36 -0500

    update ngihca modal

--- ngiv/quick/modal-ngihca.php
+++ ngiv/quick/modal-ngihca.php
@@ -7,17 +7,17 @@
       </div> <!-- /.modal-header-->
       <div class="modal-body">
         	<h1>Welcome!</h1>
-        	<p class="lede">Are you interested in starting your journey to become a professional chef? Visit <a href="http://ngihca.edu">Natural Gourmet Institute for Health &amp; Culinary Arts</a> to find information on Chef's Training Program curriculum, start dates and tuition. Alumni and resaurants looking to hire should also click Chef's Training Program.</p>
+        	<p class="lede">Are you interested in starting your journey to become a professional chef? Visit <a href="http://ngihca.edu">Natural Gourmet Institute for Health &amp; Culinary Arts</a> to find information on Chef's Training Program curriculum, start dates and tuition. Alumni and restaurants looking to hire should also click Chef's Training Program.</p>
 
         	<p class="lede">Just a healthy cooking enthusiast? Visit <a href="#" data-dismiss="modal">Natural Gourmet Institute</a> for recreational classes, certificate programs and our famous vegan Friday Night Dinner.</p>
 
         	<div class="row leader">
         		<div class="col-md-6">
-        			<a class="btn btn-block btn-primary" data-dismiss="modal">Institute</a>
+        			<a class="btn btn-block btn-primary" data-dismiss="modal">Recreational</a>
         		</div> <!-- /.col-md-6 -->
 
         		<div class="col-md-6">
-        			<a href="http://ngihca.edu" class="btn btn-block btn-primary">Chef's Training Program</a>
+        			<a href="http://ngihca.edu" class="btn btn-block btn-primary">Professional</a>
         		</div> <!-- /.col-md-6 -->
         	</div> <!-- /.row -->
         </div> <!-- /.modal-body-->
