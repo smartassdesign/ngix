510ce5c64f196a734d92937556a57026bfb3b7e9
hechtron <jon@smartassdesign.com>
2016-01-15 09:13:02 -0500

    update analytics tracking code

--- hca/footer.php
+++ hca/footer.php
@@ -53,7 +53,7 @@ jQuery(document).ready(function($){
 
 <script>
     //Google Analytics
-    var _gaq=[['_setAccount','UA-36811537-1'],['_trackPageview']];
+    var _gaq=[['_setAccount','UA-36811537-2'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
