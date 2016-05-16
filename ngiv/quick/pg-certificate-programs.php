ffeafb6a14fbc8f283cf98aead7dfbf3527fd9a4
Jon Hecht <jhecht@smartassdesign.com>
2016-05-16 16:27:47 -0400

    add'tl food blogging edit

--- ngiv/quick/pg-certificate-programs.php
+++ ngiv/quick/pg-certificate-programs.php
@@ -304,7 +304,7 @@
 					<p class="trailer"><a href="/request-info-writing-for-food-media/" class="btn-text">Request Info <span class="icon-arrow-right"></span></a></p>
 
 					<div class="well">
-						<p>To register for Writing for Food Media click enroll and submit the deposit payment. Once your deposit is received your place on the roster will be held for 1 week.  You will receive an email confirmation and link to download payment plan selection form. There will be 2 additional payments of $1,914 (includes a 6% interest fee) due on the first day of class and at the midpoint. To confirm your place in the class	 and complete registration, fill out the form and return to <a href="mailto:certificate@nginyc.com">certificate@nginyc.com</a>.</p>
+						<p>To register for Food Blogging Certificate Program click enroll and submit the deposit payment. Once your deposit is received your place on the roster will be held for 1 week.  You will receive an email confirmation and link to download payment plan selection form. There will be 2 additional payments of $1,914 (includes a 6% interest fee) due on the first day of class and at the midpoint. To confirm your place in the class	 and complete registration, fill out the form and return to <a href="mailto:certificate@nginyc.com">certificate@nginyc.com</a>.</p>
 
 						<p>Refund policy: Students completing less than 50% of the coursework will receive a refund based on the number of course hours completed less the deposit, which is nonrefundable 2 weeks prior to your start date.  Students completing more than 50% of the program are not eligible for a refund.</p>
 					</div> <!-- /.well -->					
