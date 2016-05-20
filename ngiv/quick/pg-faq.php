04f23794119dd7de3e5766a7902e151d8c4db6ef
Jon Hecht <jhecht@smartassdesign.com>
2016-05-20 12:53:51 -0400

    add pg faq

---
+++ ngiv/quick/pg-faq.php
@@ -0,0 +1,91 @@
+<div class="row">
+<div class="col-md-8">
+
+<h2>Professional Chef's Training Program FAQ’s</h2>
+
+<p class="q">How long is the Chef’s Training Program?</p>
+<p>The Chef’s Training Program is 619 hours of professional training, including the 100-hour internship.  Full-time programs are about 5.5 months long, and part-time programs are about 11.5 months long, depending on your start date.</p>
+
+<p class="q">What is the schedule for Full-time classes?</p>
+<p>Full-time classes meet Monday – Thursday from 9am – 4pm, and Friday from 9am – 1pm. </p>
+
+<p class="q">What is the schedule for Part-time classes? </p>
+<p>Part-time classes meet one weekday evening from 6 – 10pm, and one weekend day from 9am – 4pm.  Classes meet on either Sunday and Monday; Sunday and Tuesday; or Saturday and Wednesday, depending on your start date. Your class meeting dates remain the same throughout the program.</p> 
+
+<p class="q">Are there vacation days and holidays? </p>
+<p>There are no classes on federal holidays and the week between Christmas and New Year’s, in addition to a few other holidays throughout the year. <a href="/assets/NGIHCA-2016-2017-Holidays.pdf">Click here</a> for the full list of holidays.</p> 
+
+<p class="q">How many students are in a class? </p>
+<p>There is a maximum of 16 students per class. </p>
+
+<p class="q">Is the program exclusively vegan or vegetarian? </p>
+<p>While the program is mostly plant-based, we do work with specific animal proteins including dairy, eggs, poultry, finfish and shellfish. Students who do not eat these items (or any others) may choose not to taste or handle these ingredients without penalty, but will be responsible for any information shared in the class.</p> 
+
+<p class="q">Do students have to do an internship to graduate? </p>
+<p>Yes, all students must complete their internship in a professional kitchen in New York State and submit all required paperwork before the program end date in order to graduate.</p>
+
+<p class="q">What credential do students receive at the end of the program?</p>
+<p>Students who meet the requirements for graduation will receive a Diploma in Culinary Arts. </p>
+
+<p class="q">Is NGIHCA accredited and licensed? </p>
+<p>Yes, NGIHCA is accredited by ACCET.  For more information, visit ACCET.org. The school is licensed by the New York State Education Department’s Bureau of Proprietary (Career Training) School Supervision (NYSED BPSS). To learn more about NYSED BPSS visit nysed.gov.</p> 
+
+<p class="q">Does NGIHCA offer distance or online classes? </p>
+<p>At this time, all programing is offered on-site in New York City only. </p>
+
+<p class="q">What happens if a student needs to take time out of classes? </p>
+<p>Students may make up classes according to the class make-up policy. Students needing an extended leave may request a leave of absence from the Senior Director of Chef’s Training and Student Affairs. </p>
+
+<p class="q">What do students do after the program? </p>
+<p>Students generally follow three career paths: private chefs, restaurant cooks/chefs, food entrepreneurs.  Some other paths include food media and writing, education, policy and advocacy, and health coaching. You can learn more about our alumni by visiting <a href="https://www.ngihca.edu/alumni">www.ngihca.edu/alumni</a>.</p>
+
+<p class="q">Does NGIHCA have career services?</p> 
+<p>Yes, students may meet with our Career Services manager at any time during the program or after graduation. Graduates have lifetime access to our alumni-only online job boards where restaurants, caterers, private chef clients, and more post job listings to hire graduates of the Chef’s Training Program.</p>
+
+<p class="q">Is student housing offered? </p>
+NGIHCA provides a list of housing resources and recommendations and will connect enrolled students with their classmates to make housing connections. Please email <a href="mailto:admissions@nginyc.com">admissions@nginyc.com</a> for that information.
+
+<p class="q">Does NGIHCA accept international students?</p>
+<p>Yes, NGIHCA accepts international students for the full-time Chef’s Training Program who are eligible for M1 visas.  In some cases, international students may enroll in the program on an existing visa such as a spousal visa. Students with existing visas should contact the admissions team for more information. </p>
+
+<h2>Finance &amp; Tuition</h2>
+
+<p class="q">What is the difference between full-time and part-time programs?</p>
+<p>Program duration is the only differentiator between programs. The full-time programs are about 5.5 months long, and the part-time programs are about 11.5 months long, depending on your start date. Curriculum, instruction and coursework are exactly the same for both formats.</p> 
+
+<p class="q">Why is there a difference between the full- and part-time tuition?</p>
+<p>As NGIHCA is not part of the Federal Financial Aid programs (TITLE IV), we strive to find ways to make our programs more financially accessible to students whenever possible. Part-time program schedules allow us to recognize cost-savings that we pass along to students in order to make the program more financially accessible.</p> 
+
+<p class="q">What does tuition cover?</p>
+<p>Tuition includes all books, materials, uniforms and 30+ piece professional knife and pastry set made by Mercer. It also includes career services, lifetime access to our alumni-only job board, and inclusion in our 2500+ worldwide alumni community.</p>
+
+<p class="q">Does NGIHCA offer scholarships?</p>
+<p>Yes, NGIHCA offers various scholarships which are open for application throughout the year. More information can be found on the scholarship page of our website. <a href="https://www.ngihca.edu/#scholarships">https://www.ngihca.edu/#scholarships</a> </p>
+
+<p class="q">Does NGIHCA offer payment plans? </p>
+<p>Yes, NGIHCA offers payment plans for both full- and part-time programs.  Students in the full-time program can choose to pay tuition in four installments following the deposit. Students in the part-time program can choose to pay tuition in three, four or 10 installments. To learn more about payment plans, contact the admissions team.</p>
+
+<p class="q">Does NGIHCA accept student loans?</p>
+<p>Yes, NGIHCA accepts Sallie Mae smart option student loans and Tuition Flex student loans.  Students can apply for Sallie Mae loans directly by visiting the <a href="https://www.salliemae.com/student-loans/career-training-smart-option-student-loan/" target="_blank">Sallie Mae website</a>. Students are encouraged to apply with a co-signer for more favorable rates and terms. </p>
+
+<p>Students wishing to learn more about Tuition Flex may contact <a href="mailto:admissions@nginyc.com">admissions@nginyc.com</a> for more information on loan eligibility and requirements. </p>
+
+<p class="q">Does NGIHCA accept the Post 9/11 GI Bill? </p>
+<p>Yes, NGIHCA accepts Post 9/11 GI Bill benefits and offers military students a $5,000 tuition waiver. Please contact <a href="mailto:admissions@nginyc.com">admissions@nginyc.com</a> for more information.</p>
+
+<h2>Applications &amp; Procedures</h2>
+
+<p class="q">When should students apply for my program?</p>
+<p>Enrollment is done on a rolling basis and students are encouraged to submit their completed applications as early as possible for the best chance of acceptance into the program of their choice.</p>
+
+<p class="q">How do students apply for the Chef’s Training Program?</p>
+<p>Applications are submitted online through <a href="https://www.ngihca.edu/application">the website</a>. Students may start the application and save for later. Only completed applications will be reviewed for enrollment.</p>
+
+<p class="q">What happens if a student’s first choice program is full? </p>
+<p>If a student’s first choice of program is not available, they may roll the application to the next available start date.  A second application fee is not required.</p> 
+
+<p class="q">How do students apply for student a visa? </p>
+<p>Students will be able to apply for their M1 visa following acceptance to the program and issuance of their I-20 form. <a href="/assets/NGIHCA-International-Student.pdf">Click here</a> to learn more about obtaining a visa.</p>
+
+</div> <!-- /.col-md-8 -->
+</div> <!-- /.row -->
