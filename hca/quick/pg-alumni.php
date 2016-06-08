510d69f821ba532296f2d629e209068451a2344d
Jon Hecht <jhecht@smartassdesign.com>
2016-06-08 11:26:55 -0400

    content added to alumni page

--- hca/quick/pg-alumni.php
+++ hca/quick/pg-alumni.php
@@ -1,5 +1,5 @@
 <section id="intro" class="p-b-lg">
-		<h1 class="large">NGI <span style="text-decoration:line-through;">Alumni</span> Rockstars: Our Global Community of Healthful Gourmets</h1>
+		<h1 class="large">NGI Alumni: A Global Community of Healthful Gourmets</h1>
 		<p>Our alumni are renowned chefs, entrepreneurs, thought leaders, teachers, authors, trailblazers. Our alumni cook in professional kitchens, for private clients and in their own homes. They hail from 45 countries - and when they’re not opening restaurants, they’re opening doors for each other.</p>
 </section> <!-- /#intro -->
 
@@ -29,6 +29,31 @@
 
 
 </div> <!-- /.pod -->
+
+<div class="row">
+
+	<div class="col-md-12">
+		<h5>Check out some more alumni profiles</h5>
+	</div> <!-- /.col-md-12 -->
+
+	<div class="col-md-4">
+		<a href="https://naturalgourmetinstitute.com/qa-with-alum-davis-lindsey-vegetable-manager-at-stone-barns/"><img src="<?php echo get_bloginfo('stylesheet_directory');?>/assets/img/ngihca-alumni-lindsey.jpg" class="img-fluid"></a>
+			<p class="m-t-1"><small><a href="https://naturalgourmetinstitute.com/qa-with-alum-davis-lindsey-vegetable-manager-at-stone-barns/">Davis Lindsey</a> used his culinary training to land the role of Field Education Manager at Stone Barns Center for Food & Agriculture in Pocantico Hills, New York.</small></p>
+	</div> <!-- /.col-md-4 -->
+
+	<div class="col-md-4">
+		<a href="https://naturalgourmetinstitute.com/chef-elyse-kopecky/"><img src="<?php echo get_bloginfo('stylesheet_directory');?>/assets/img/ngihca-alumni-kopecky.jpg" class="img-fluid"></a>
+			<p class="m-t-1"><small><a href="https://naturalgourmetinstitute.com/chef-elyse-kopecky/">Elyse Kopecky</a> and friend Shalane Flanagan, an Olympic medalist, co-wrote a cookbook for runners, titled <em>Run Fast Eat Slow: Nourishing Recipes for Athletes</em>.</small></p>
+	</div> <!-- /.col-md-4 -->	
+
+	<div class="col-md-4">
+		<a href="https://naturalgourmetinstitute.com/qa-with-ngi-alum-cara-mangini-author-of-the-vegetable-butcher/"><img src="<?php echo get_bloginfo('stylesheet_directory');?>/assets/img/ngihca-alumni-mangini.jpg" class="img-fluid">
+			<p class="m-t-1"><small><a href="https://naturalgourmetinstitute.com/qa-with-ngi-alum-cara-mangini-author-of-the-vegetable-butcher/">Cara Mangini</a> is an Ohio-based chef who recently published her first cookbook, <em>The Vegetable Butcher.</em>.</small></p>
+	</div> <!-- /.col-md-4 -->		
+
+</div> <!-- /.row -->
+
+
 
 
 <?php include("snippets/quote-jamieson.php"); ?>
