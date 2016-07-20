e771c7190104ae060ef972cb589d6b8655452e10
Jon Hecht <jhecht@smartassdesign.com>
2016-07-20 09:32:57 -0400

    add cta on faq pg

--- hca/page-faq.php
+++ hca/page-faq.php
@@ -9,7 +9,7 @@
 	<div class="page-wrap">
 
 		<div class="container">
-			<div class="row m-t-lg">
+			<div class="row m-y-lg">
 				<div class="col-md-12">
 					
 
@@ -55,6 +55,12 @@ $counter = 0;
 
 					<?php endif; ?>
 
+
+			<div class="text-xs-center m-y-md p-t-md cta">
+				<a href="#" class="btn btn-lg btn-primary-outline" data-toggle="modal" data-target="#infoModal">Request Info</a>
+				<a href="<?php bloginfo('url');?>/apply" class="btn btn-lg btn-primary">Apply Now</a>
+			</div>
+
 				</div> <!-- /.col-md-8 -->
 			</div>	<!-- /.row -->					
 
@@ -63,6 +69,24 @@ $counter = 0;
 				</div> <!-- /.col-md-12 -->
 			</div>	<!-- /.row -->
 		</div>	<!-- /.container -->
+
+
+<?php /* <div class="container-fluid bg-green p-y-lg m-t-3">
+	<div class="row no-gutter">
+		<div class="col-md-12">
+			<div class="text-xs-center">
+				<a href="#" class="btn btn-lg btn-info-outline" data-toggle="modal" data-target="#infoModal">Request Info</a>
+				<a href="<?php bloginfo('url');?>/apply" class="btn btn-lg btn-info">Apply Now</a>
+			</div>
+		</div> <!-- /.col-md-12 -->
+	</div> <!-- /.row -->
+</div> <!-- /.container-fluid -->
+*/ ?>
+
+
+
 	</div> <!-- /.page-wrap -->
+
+	<?php get_template_part('quick/modal-info'); ?>
 
 <?php get_footer(); ?>
