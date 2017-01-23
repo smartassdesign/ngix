c786e232bffc76f4483febb01828e70712f7f0e0
Jon Hecht <jhecht@smartassdesign.com>
2017-01-23 16:31:25 -0500

    fix open house time 2 issue

--- hca/quick/open-house.php
+++ hca/quick/open-house.php
@@ -13,35 +13,20 @@
 }
 ?>
 
-at <?php the_field('open_house_time', 'option'); ?>
-
-
-<?php if(get_field('open_house_date', 'option'))
-{
-
-	$date = DateTime::createFromFormat('Ymd', get_field('open_house_date_two', 'option'));
-			echo ' and ';
-			echo $date->format('F j, Y');
-}
-?>
-
-at <?php the_field('open_house_time_two', 'option'); ?>
-
-</h4>
+at <?php the_field('open_house_time', 'option'); ?></h4>
 
 			<!-- <h4>Monday, October 12 at 6pm</h4> -->
 			<p class="lede">Whether you&rsquo;re weighing a career in the culinary arts or researching schools to find the best program for you, join us and figure it all out.</p>
 
-			<br />
-
+			<br>
 			<p class="small">Licensed by the State of New York and accredited by ACCET</p>
 
 		</div> <!-- /.col-md-9 -->
 
 		<div class="col-md-4">
 			<?php echo do_shortcode('[gravityform id="5" name="Open House 7/8 Test" title="false" description="false" ajax="true"]') ?>
- 		</div> <!-- /.col-md-4 -->
+		</div> <!-- /.col-md-4 -->
 
 		</div> <!-- /.row -->
 	</div> <!-- /.container -->
-</div> <!-- /#open-house .pod -->
++</div> <!-- /#open-house .pod -->
