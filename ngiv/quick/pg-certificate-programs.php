194ff7e97c3e820a45239b44c69f459c6cec0a21
Jon Hecht <jhecht@smartassdesign.com>
2017-06-12 09:06:17 -0400

    update certificate programs w food therapy 2x

--- ngiv/quick/pg-certificate-programs.php
+++ ngiv/quick/pg-certificate-programs.php
@@ -1,5 +1,5 @@
 <h1 class="large">Certificate Programs</h1>
-<p class="lede certificate-subnav">As the leader in health-supportive culinary education, we understand that students are interested in many aspects of the food system. Natural Gourmet Institute’s interactive and dynamic Certificate Programs are designed to give students the opportunity to gain first-hand experience and expertise in the areas of <a href="#food-therapy">Food Therapy</a>, <a href="#starting-a-food-business">Starting a Food Business</a>, <a href="#sustainable-farming">Sustainable Farming</a>, <a href="#culinary-nutrition">Culinary Nutrition</a> and <a href="#food-blogging">Food Blogging</a>. By working with accomplished chefs, industry experts and partners, the Certificate Programs allow students to further develop their knowledge and gain new perspectives on exciting and essential components of the better food movement.</p>
+<p class="lede certificate-subnav">As the leader in health-supportive culinary education, we understand that students are interested in many aspects of the food system. Natural Gourmet Institute’s interactive and dynamic Certificate Programs are designed to give students the opportunity to gain first-hand experience and expertise in the areas of <a href="#food-therapy">Food Therapy</a>, <a href="#sustainable-farming">Sustainable Farming</a> and <a href="#culinary-nutrition">Culinary Nutrition</a>. By working with accomplished chefs, industry experts and partners, the Certificate Programs allow students to further develop their knowledge and gain new perspectives on exciting and essential components of the better food movement.</p>
 &nbsp;
 
 
@@ -46,19 +46,23 @@
 
 					
 				<div class="col-md-4">
-					<a href="/certificate-programs/?add-to-cart=26503" rel="nofollow" data-product_id="26503" data-product_sku="" class="btn btn-primary btn-block">Enroll</a>
+					<a href="/certificate-programs/?add-to-cart=26503" rel="nofollow" data-product_id="26503" data-product_sku="" class="btn btn-primary btn-block trailer">Enroll in July Session</a>
+					<a href="/certificate-programs/?add-to-cart=29003" rel="nofollow" data-product_id="29003" data-product_sku="" class="btn btn-primary btn-block">Enroll in August Session</a>
 					<aside class="well">
 						<h5>Dates</h5>
-						<p>July 10, 2017 &mdash; July 21, 2017<br>
+						<p><b>July Session</b><br />
+						July 10, 2017 &mdash; July 21, 2017<br>
 						Monday &mdash; Friday<br />
 						9:30am &mdash; 4:30pm<br></p>
+
+						<p><b>August Session</b><br />
+						August 21, 2017 &mdash; September 1, 2017<br>
+						Monday &mdash; Friday<br />
+						9:30am &mdash; 4:30pm<br></p>						
 						<h5>Cost</h5>
 						<p>$5,550 / Deposit: $1,833</p>
-						<!-- <p>Payment plans are available. Please email <a href="mailto:certificate@nginyc.com">certificate@nginyc.com</a> for more information.</p> --> 
-
 						<p>Students will receive a certificate of completion.</p>
-
-					</aside> <!-- /.well -->
+					</aside> <!-- /.well -->				
 				</div> <!-- /.col-md-4 -->
 				
 			</div> <!-- /.row -->		
@@ -66,75 +70,6 @@
 	</div>	
 
 
-
-		<div class="sub-pod">
-	<section id="starting-a-food-business">
-			<img src ="/assets/food-entrepreneurship.jpg" alt="Starting a Food Business: How to Plan, Execute and Market Your Idea" class="img-fluid trailer">
-
-			<div class="row">
-				<div class="col-md-8">
-					<header>	
-			      		<h2>Starting a Food Business: How to Plan, Execute and Market Your Idea</h2>
-					</header>
-
-					<!-- <h4>Overview</h4> -->
-					<p>Ready to transform your passion for food into an exciting new business? This fall, learn the recipe for success from some of New York’s best food and business entrepreneurs under the guidance of industry experts. Led by experienced food start-up founder and advisor Jennifer Goggin, students will learn what it takes to start a successful food business from established food entrepreneurs. Past guest speakers have included Alison Gilbert, Project AG; Lynn Yoder Thibault, Amazon Fresh; and Christine Rico, Speed Dial.</p>
-
-					<p>Throughout this 4 session evening course, you will learn the fundamentals of what to expect when launching a food business.</p>
-
-					<p>In addition to gaining real world experience, receiving expert advice, and hearing war stories from seasoned food entrepreneurs, you will also:</p>
-
-					<ul class="trailer">
-						<li>Gain an understanding of key business concepts and organizational structures</li>
-						<li>Learn techniques to strengthen your business concept </li>
-						<li>Basic finance skills: determining feasibility and reading financials</li>
-						<li>Sales and Marketing: Best practices to tell your story</li>
-						<li>Defining your mission: Driving impact, instilling mission, vision, values, and selecting partners</li>
-					</ul>		
-
-					<h5 class="leader">Notable Guest Speakers</h5>
-
-					<p><b>David Benzaquen</b> is the Founder and CEO of <a href="http://plantbasedsolutions.com/" target="_blank">PlantBased Solutions</a>, an award-winning, mission-driven, strategic brand management and marketing agency for plant-based consumer products businesses. PlantBased Solutions helps companies launch and grow their businesses through marketing, branding, fundraising and more. David is also a sought after natural products industry expert and has spoken about the growing plant-based economy at academic and industry conferences throughout the world. He serves as a mentor and advisor to multiple food incubators, accelerators and venture capital firms, and is a contributing writer with several prominent natural industry trade publications.</p>
-
-					<p><b>Alison Gilbert</b>, founder of <a href="http://www.project-ag.co/" target="_blank">Project AG</a>, is a pastry chef turned COO turned business consultant and coach. Alison is passionate about helping emerging entrepreneurs build thriving businesses that in turn fuel economic and cultural growth. Prior to building Project AG, Alison was hire four and COO of food media company Tasting Table where she helped grow it into a forty person, multi million dollar company reaching a readership of millions. To date she’s helped 30+ businesses across commerce, media, tech, design and fashion successfully scale including such brands as Well+Good, Levo League, Maiyet, Quinciple and Plum Alley.</p>
-
-					<p><b>Rose DeStefano</b> leads the Best for NYC campaign, a partnership between B Lab, the non-profit certifying B Corporations, and the NYC Mayor's Office of Workforce Development to advance the social business movement from a grassroots and policy-level perspective. As a Senior B Impact fellow, she has worked with hundreds of companies across New York's five boroughs to improve their social and environmental impact. Rose received her GreenMBA from Dominican University in California, where she led Spirit Rock Meditation Center in their California Green Business Certification and consulted for Bay Area Honey Company, Love a Bee. Before transitioning into business, Rose managed the fundraising departments for non-profits in India and California with budgets exceeding $5 million+.</p>
-
-
-					<h5 class="leader">Instructor</h5>
-					<p><b>Jennifer Goggin</b> (Founder of <a href="http://www.pippinfoods.com/" target="_blank">Pippin Foods</a> and Co-Founder of <a href="http://www.farmersweb.com/" target="_blank">Farmers Web</a>) has been an entrepreneur and advisor in the food-tech space since 2011 when she co-founded FarmersWeb to help farms, food hubs and food artisans streamline wholesale orders, deliveries and payments online. Prior to FarmersWeb, Jennifer was Director of Operations at Basis Farm to Chef, a local food distributor in New York. She has been a featured panelist and moderator for conferences on entrepreneurship and the food technology industry, as well as a guest columnist for <em>Food+Tech Connect</em> and <em>Huffington Post Food</em>. Jennifer is a board member of Slow Food NYC where she works on its Snail of Approval committee to recognize restaurants, bars, and markets that contribute to the quality, authenticity and sustainability of the New York City food supply.</p>					
-
-					<p class="trailer"><a href="/request-info-starting-a-food-business/" class="btn-text">Request Info <span class="icon-arrow-right"></span></a></p>
-
-					<div class="well">
-						<p>This certificate program is offered for personal enrichment only.</p>
-						<p>To register for Starting a Food Business click enroll and submit the deposit payment. Once your deposit is processed you will receive an email confirmation and link to complete the balance of tuition payment form. Follow the instructions in the form to complete your registration.</p>
-
-						<p>Tuition and deposit are nonrefundable 3 weeks prior to your start date. Students joining the program late, leaving the program early, or not participating in all activities will not be eligible for discounts or partial refunds. In the event of a program cancelation by Natural Gourmet Institute, all money will be refunded to the student.</p>
-					</div> <!-- /.well -->							
-				</div> <!-- /.col-md-8 -->	
-
-					
-				<div class="col-md-4">
-
-					<!-- <a href="/request-info-food-entrepreneurship/" class="btn btn-primary btn-block">Request Info</a> -->
-					<a href="/certificate-programs/?add-to-cart=23796" rel="nofollow" data-product_id="23796" data-product_sku="" class="btn btn-primary btn-block disabled" >Unavailable</a>
-					<aside class="well">
-						<h5>Dates</h5>
-						<p>New dates coming soon. Click Request Info below to be notified of future course dates</p>
-						<!-- <p>May 3, 2017 &mdash; May 24, 2017 <br />
-						Wednesday<br />
-						6:30pm &mdash; 9:30pm</p>
-						<h5>Cost</h5>
-						<p>$1,200 / Deposit: $600</p>
-
-						<p>Students will receive a certificate of completion.</p> -->
-					</aside> <!-- /.well -->
-				</div> <!-- /.col-md-4 -->
-				
-			</div> <!-- /.row -->	
-	</section> <!-- /#starting-a-food-business -->
-	</div>	
 
 
 
@@ -275,185 +210,3 @@
 		</section> <!-- /#culinary-nutrition -->		
 		</div>			
 
-
-
-
-
-
-
-
-	<div class="sub-pod">
-		<section id="food-blogging">
-
-			<img src ="/assets/food-blogging.jpg" alt="Food Blogging Certificate Program – Creating, Managing and Growing a Professional Food Blog " class="img-fluid trailer">			
-			<div class="row">
-				<div class="col-md-8">
-					<header>	
-			      		<h2>Food Blogging Certificate Program – Creating, Managing and Growing a Professional Food Blog </h2>
-					</header>
-
-					<!-- <h4>Overview</h4> -->
-					<p>Ever wonder how your favorite food blog went from passion project to profession? Maybe you have been looking for a way to do just that with your blog, or maybe you are just getting started and want expert guidance on creating a professional food blog?</p>
-
-					<p>Join Phoebe Lapine (creator of <a href="http://feedmephoebe.com/" target="_blank">Feed Me Phoebe,</a> author of <em>In the Small Kitchen: 100 Recipes from Our Year of Cooking in the Real World)</em> to learn how she turned her passion for healthy beautiful food into a successful career. In this lecture-based course, you will learn the fundamentals of creating a successful food blog, from creating content to building an audience to generating income. This course is ideal for anyone with an interest in food blogging - currently managing or thinking about starting a food blog.</p> 
-
-					<p>You will gain practical skills and knowledge and, most importantly, gain new tools grow your audience and generate revenue. Over 5 sessions, Phoebe will expertly guide the class through the below topics:</p>
-
-
-					<b>Getting Started – Finding your voice and perspective </b>
-					<ul><li>Choosing a name and look</li>
-					<li>Design and development tips and tricks</li>
-					<li>Plugins and widgets: getting the most out of your site</li>
-					<li>Cost overhead: the most important aspects to invest in your blog</li></ul>
-
-					<b>Content is King – Managing content for impact and engagement</b>
-					<ul><li>Developing your voice</li>
-					<li>The importance of an editorial calendar</li>
-					<li>Recipe development; how to adapt and accredit</li>
-					<li>Best practices to build an engaged audience </li></ul>
-
-					<b>Food Photography – A picture is worth a 1,000 words, what are your photos saying?</b>
-					<ul><li>How to light, compose and edit your best food photos</li></ul>
-
-					<b>Community and Growing Your Site – Technical advice and community building tips</b>
-					<ul><li>Using Google Analytics, SEO, social media and more to promote your site and grow your audience</li>
-					<li>Reciprocity and building a community </li>
-					<li>Methods to build an offline community</li>
-					<li>Getting your name out: guest posting and freelancing</li></ul>
-
-					<b>Hobby to Career – Professional tools to start earning money from your blog</b>
-					<ul><li>Advertising, sponsored posts, brand collaborations and more</li>
-					<li>Getting a cookbook deal</li>
-					<li>Bridge jobs and other ways to support your career</li></ul>
-
-					<p class="trailer"><a href="/request-info-food-blogging/" class="btn-text">Request Info <span class="icon-arrow-right"></span></a></p>
-
-					<div class="well">
-						<p>This certificate program is offered for personal enrichment only.</p>
-						<p>To register for Food Blogging Certificate Program click enroll and submit the deposit payment. Once your deposit is received your place on the roster will be held for 1 week.  You will receive an email confirmation and link to download payment plan selection form. To confirm your place in the class and complete registration, fill out the form and return to <a href="mailto:certificate@nginyc.com">certificate@nginyc.com</a>.</p>
-
-						<p>Refund policy: Students completing less than 50% of the coursework will receive a refund based on the number of course hours completed less the deposit, which is nonrefundable 2 weeks prior to your start date.  Students completing more than 50% of the program are not eligible for a refund.</p>
-					</div> <!-- /.well -->					
-
-				</div> <!-- /.col-md-8 -->	
-
-					
-				<div class="col-md-4">
-					<a href="/certificate-programs/?add-to-cart=19774" rel="nofollow" data-product_id="19774" data-product_sku="" class="btn btn-primary btn-block disabled">Coming Soon</a>
-					<aside class="well">
-						<h5>Dates</h5>
-						<p>New dates coming soon. Click Request Info below to be notified of future course dates</p>						
-<!-- 						<p>Monday, 9/12: 6:30pm &mdash; 10pm<br/>
-						Tuesday, 9/13: 6:30pm &mdash; 10pm<br/>
-						Tuesday, 9/20: 6:30pm &mdash; 10pm<br/>
-						Thursday, 9/22: 6:30pm &mdash; 10pm<br/>
-						Monday, 9/26: 6:30pm &mdash; 10pm</p> -->
-						<h5>Cost</h5>
-						<p>Full Tuition: $2,400<br />
-						Deposit due at registration: $800</p>
-						<!-- <p>Payment plans are available. Please email <a href="mailto:certificate@nginyc.com">certificate@nginyc.com</a> for more information.</p> --> 
-
-						<p>Students will receive a certificate of completion.</p>
-
-					</aside> <!-- /.well -->
-				</div> <!-- /.col-md-4 -->
-				
-			</div> <!-- /.row -->		
-		</section> <!-- /#food-media -->
-	</div>	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php /*
-		<div class="sub-pod">
-			<section id="culinary-nutrition-la">
-			<img src ="/assets/NGI-Culinary-Nutrition-LA.jpg" alt="NGI Culinary Nutrition Certificate Program Los Angeles" class="img-fluid trailer">
-
-			<div class="row">
-				<div class="col-md-8">
-					<header>	
-			      		<h2>Culinary Nutrition Certificate Program - New! Los Angeles Location</h2>
-					</header>
-
-					<!-- <h4>Overview</h4> -->
-					<p class="trailer">The Culinary Nutrition Certificate Program with Natural Gourmet Institute provides students with an in-depth and hands-on understanding of nutrition theory. The program explores the science of nutrition, nutrition in the marketplace, trends, and specialized diets through lecture and kitchen labs. Led by Kayleen St. John, MS RD, students will learn from expert guest speakers from the food, health, and nutrition fields!</p>
-
-<p>The program will be held at Cook LA, in Studio City, Los Angeles. Upon completion of the course, students will have learned how to incorporate the fundamentals of nutrition into their culinary perspective.
-</p><br>
-						
-					<h4>Coursework</h4>
-						<b>Fundamentals in Nutrition</b>
-						<ul>
-							<li>Food-based micronutrients and macronutrients</li>
-							<li>Optimizing nutrients through food</li>
-							<li>Nutrient retention</li>
-							<li>Food synergies</li>
-							<li>Nutrient analysis of menus</li>
-							<li>Nutrition in the marketplace</li>
-						</ul>
-						<b>Specialized Diets</b>
-						<ul>
-							<li>Life cycle nutrition</li>
-							<li>Vegetarian and vegan nutrition</li>
-							<li>Diabetes and metabolic syndrome</li>
-							<li>Food allergies and intolerances</li>
-							<li>Gluten sensitivity/intolerance</li>
-						</ul>
-						<b>Trends in Nutrition</b>
-						<ul>
-							<li>Alternative diets</li>
-							<li>Weight management</li>
-							<li>Inflammation</li>
-							<li>Econutrition</li>
-						</ul>
-
-						<p class="trailer"><a href="/request-info-culinary-nutrition-la/" class="btn-text">Request Info <span class="icon-arrow-right"></span></a></p>
-
-					<div class="well">
-						<p>To register for Culinary Nutrition click enroll and submit the deposit payment. Once your deposit you will receive an email confirmation and link to download payment plan selection form. To confirm your place in the class and complete registration, fill out the form and return to <a href="mailto:certificate@nginyc.com">certificate@nginyc.com</a>.</p>
-
-						<p>Refund policy: Tuition and deposit are nonrefundable 3 weeks prior to your start date. Students joining the program late, leaving the program early, or not participating in all activities will not be eligible for discounts or partial refunds. In the event of a program cancelation by Natural Gourmet Institute, all money will be refunded to the student.</p>
-
-					</div> <!-- /.well -->	
-
-				</div> <!-- /.col-md-8 -->	
-
-					
-				<div class="col-md-4">
-					<a href="/certificate-programs/?add-to-cart=13430" rel="nofollow" data-product_id="6747" data-product_sku="" class="btn btn-primary btn-block disabled">In Session</a>
-					<aside class="well">
-						<h5>Dates</h5>
-						<p>February 15, 2016 &mdash; February 26, 2016 9:00am &mdash; 4:00pm<br>
-						Monday &mdash; Friday </p>
-						<h5>Cost</h5>
-						<p>$5,550 / Deposit: $1,833</p>
-						<p>Students will receive a certificate of completion.</p>
-					</aside>
-
-				</div> <!-- /.col-md-4 -->
-				
-			</div> <!-- /.row -->
-		</section> <!-- /#culinary-nutrition -->		
-		</div>			
-
-
-*/ ?>
