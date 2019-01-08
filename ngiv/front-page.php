d385cea9feefffdb3d170d82a09ec2f30cedb542
hechtron <jon@smartassdesign.com>
2019-01-08 10:45:04 -0500

    adjust closeout sizing and linkage

--- ngiv/front-page.php
+++ ngiv/front-page.php
@@ -11,7 +11,7 @@
 		<!-- START HOME CONTENT HERE (No containers, so everyone's on his own.) -->
 	
 	<section id="closeout">
-		<div class="the-notice" style="background:#262626; width: 100%; padding: 30px; "><h2 style="margin-bottom: 0; color:#f6f6f6;">The Natural Gourmet Institute’s health-supportive, plant-based curriculum is now exclusively offered at the Institute of Culinary Education. <a href="https://www.ice.edu/newyork/career-programs/natural-gourmet-center?mcid=62897">Learn more about career training in Health-Supportive Culinary Arts at the new Natural Gourmet Center at ICE.</a></h2></div>
+		<div class="the-notice" style="background:#262626; width: 100%; padding: 40px; "><h3 style="color:#f6f6f6; line-height: 1.3">The Natural Gourmet Institute’s health-supportive, plant-based curriculum is now exclusively offered at the Institute of Culinary Education. Learn more about career training in Health-Supportive Culinary Arts at the new Natural Gourmet Center at ICE.</h3><a href="https://www.ice.edu/newyork/career-programs/natural-gourmet-center?mcid=62897" class="btn btn-primary btn-sm">Click here</a></div>
 	</section>
 
 		<section id="mission" class="pod">
