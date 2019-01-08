cee191cc612ada73c439353ddac81d8a98d4ba23
hechtron <jon@smartassdesign.com>
2019-01-08 17:33:57 -0500

    add ice info to contact page

--- ngiv/quick/pg-contact.php
+++ ngiv/quick/pg-contact.php
@@ -10,6 +10,14 @@
 			Phone: <a href="tel://1-212-645-5170">+212 645 5170</a><br>
 			Fax: <a href="tel://1-212-989-1493">+212 989 1493</a><br></p>
 			<p><a href="https://www.google.com/maps/dir//Natural+Gourmet+Institute,+48+W+21st+St+%232,+New+York,+NY+10010" target="_blank" class="btn-text">Get directions <span class="icon-arrow-right"></span></a></p>
+			
+			<h4>New Natural Gourmet Center</h4>
+			<p>Institute of Culinary Education<br />
+			225 Liberty Street, 3rd Floor<br />
+			New York, NY 10281<br />
+				Phone: <a href="tel://1-888-574-2433">+888 574 CHEF</a><br /></p>
+			
+			<p><a href="https://www.google.com/maps/dir/40.6696678,-73.9809228/Institute+Of+Culinary+Education,+Liberty+Street,+New+York,+NY/@40.6907927,-74.0151941,14z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x89c259a462029e95:0x4797265df5f2112e!2m2!1d-74.0152461!2d40.7120079!3e3" target="_blank" class="btn-text">Get directions <span class="icon-arrow-right"></span></a></p>
 		</div> <!-- /col-md-4 -->
 	
 		<div class="col-md-8 trailer">
