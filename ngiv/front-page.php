fcf647851d6bbef731c91599fa6555e86947aa71
hechtron <jon@smartassdesign.com>
2019-01-10 14:03:33 -0500

    drop closeout from front page, move to header

--- ngiv/front-page.php
+++ ngiv/front-page.php
@@ -9,10 +9,7 @@
 <div class="page-wrap raw">
 
 		<!-- START HOME CONTENT HERE (No containers, so everyone's on his own.) -->
-	
-	<section id="closeout">
-		<div class="the-notice" style="background:#262626; width: 100%; padding: 40px; "><h3 style="color:#f6f6f6; line-height: 1.3">The Natural Gourmet Institute’s health-supportive, plant-based curriculum is now exclusively offered at the Institute of Culinary Education. Learn more about career training in Health-Supportive Culinary Arts at the new Natural Gourmet Center at ICE.</h3><a href="https://www.ice.edu/newyork/career-programs/natural-gourmet-center?mcid=62897" class="btn btn-primary btn-sm">Click here</a></div>
-	</section>
+
 
 		<section id="mission" class="pod">
 			<div class="container">
